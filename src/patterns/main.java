6a67b88d93208a9a0b41502af5c1a60623a12df7
khushii-19 <khushi97000@gmail.com>
2025-02-05 22:18:56 +0530

    Patterns added

---
+++ src/patterns/main.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class main {
+    public static void main(String[] args) {
+        pattern1(5);
+    }
+    static void pattern1(int n) {
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1;j <=i; j++) {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
