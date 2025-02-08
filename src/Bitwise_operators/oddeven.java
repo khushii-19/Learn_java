e402417791748fecb136931c5cfb7f6ea5ab5bd9
khushii-19 <khushi97000@gmail.com>
2025-02-08 15:47:01 +0530

    Bitiwse operators practice

---
+++ src/Bitwise_operators/oddeven.java
@@ -0,0 +1,11 @@
+package Bitwise_operators;
+
+public class oddeven {
+    public static void main(String[] args) {
+        int n = 67;
+        System.out.println(isodd(n));
+    }
+    private static boolean isodd(int n) {
+        return (n&1) ==1;
+    }
+}
